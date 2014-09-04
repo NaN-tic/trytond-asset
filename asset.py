04b002e9d0a11ba0bce2f8af556ce49bc7f88052
?ngel ?lvarez <angel@nan-tic.com>
2014-09-04 12:43:42 +0200

    initial commit

---
+++ asset.py
@@ -0,0 +1,65 @@
+#This file is part of Tryton.  The COPYRIGHT file at the top level of
+#this repository contains the full copyright notices and license terms.
+from trytond.pool import Pool
+from trytond.model import ModelSQL, ModelView, fields
+from trytond.pyson import Eval
+
+__all__ = ['Asset']
+
+
+class Asset(ModelSQL, ModelView):
+    'Asset'
+    __name__ = 'asset'
+    name = fields.Char('Name', required=True, select=True)
+    code = fields.Char('Code', required=True, select=True,
+        states={
+            'readonly': Eval('code_readonly', True),
+            },
+        depends=['code_readonly'])
+    code_readonly = fields.Function(fields.Boolean('Code Readonly'),
+        'get_code_readonly')
+    product = fields.Many2One('product.product', 'Product', required=True,
+        domain=[
+            ('type', '=', 'assets'),
+            ])
+    active = fields.Boolean('Active')
+
+    @classmethod
+    def __setup__(cls):
+        super(Asset, cls).__setup__()
+        cls._sql_constraints = [
+            ('code_uniq', 'UNIQUE(code)',
+                'The code of the asset must be unique.')
+            ]
+
+    @staticmethod
+    def default_active():
+        return True
+
+    @staticmethod
+    def default_code_readonly():
+        Configuration = Pool().get('asset.configuration')
+        config = Configuration(1)
+        return bool(config.asset_sequence)
+
+    def get_code_readonly(self, name):
+        return True
+
+    @classmethod
+    def search_rec_name(cls, name, clause):
+        return ['OR',
+            ('code',) + tuple(clause[1:]),
+            ('name',) + tuple(clause[1:]),
+            ]
+
+    @classmethod
+    def create(cls, vlist):
+        Sequence = Pool().get('ir.sequence')
+        Configuration = Pool().get('asset.configuration')
+
+        vlist = [x.copy() for x in vlist]
+        for values in vlist:
+            if not values.get('code'):
+                config = Configuration(1)
+                values['code'] = Sequence.get_id(config.asset_sequence.id)
+        return super(Asset, cls).create(vlist)
---
+++ tests/test_asset.py
@@ -0,0 +1,26 @@
+#This file is part of Tryton.  The COPYRIGHT file at the top level of
+#this repository contains the full copyright notices and license terms.
+import unittest
+import trytond.tests.test_tryton
+from trytond.tests.test_tryton import test_view, test_depends
+
+
+class AssetTestCase(unittest.TestCase):
+    'Test module'
+
+    def setUp(self):
+        trytond.tests.test_tryton.install_module('asset')
+
+    def test0005views(self):
+        'Test views'
+        test_view('asset')
+
+    def test0006depends(self):
+        'Test depends'
+        test_depends()
+
+
+def suite():
+    suite = trytond.tests.test_tryton.suite()
+    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(AssetTestCase))
+    return suite
