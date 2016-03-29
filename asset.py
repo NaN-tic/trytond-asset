ec2f3377ef9d87eaa8a6900382a5a5279a744515
Sergi Almacellas Abellana <sergi@koolpi.com>
2016-03-29 09:52:10 +0200

    Add company field and access rule

--- asset.py
+++ asset.py
@@ -1,9 +1,10 @@
-#This file is part of Tryton.  The COPYRIGHT file at the top level of
-#this repository contains the full copyright notices and license terms.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 from trytond.pool import Pool
 from trytond.model import ModelSQL, ModelView, fields, Unique
-from trytond.pyson import Eval
+from trytond.pyson import Eval, If
 from trytond.transaction import Transaction
+from trytond import backend
 
 __all__ = ['Asset']
 
@@ -11,6 +12,12 @@ __all__ = ['Asset']
 class Asset(ModelSQL, ModelView):
     'Asset'
     __name__ = 'asset'
+    company = fields.Many2One('company.company', 'Company', required=True,
+        select=True,
+        domain=[
+            ('id', If(Eval('context', {}).contains('company'), '=', '!='),
+                Eval('context', {}).get('company', -1)),
+            ])
     name = fields.Char('Name')
     code = fields.Char('Code', required=True, select=True,
         states={
@@ -36,6 +43,25 @@ class Asset(ModelSQL, ModelView):
             ('code_uniq', Unique(t, t.code),
                 'The code of the asset must be unique.')
             ]
+
+    @classmethod
+    def __register__(cls, module_name):
+        TableHandler = backend.get('TableHandler')
+        pool = Pool()
+        Company = pool.get('company.company')
+        cursor = Transaction().cursor
+        table = TableHandler(cursor, cls, module_name)
+        sql_table = cls.__table__()
+        company_table = Company.__table__()
+        created_company = not table.column_exist('company')
+
+        super(Asset, cls).__register__(module_name)
+
+        # Migration: new company field
+        if created_company:
+            # Don't use UPDATE FROM because SQLite nor MySQL support it.
+            value = company_table.select(company_table.id, limit=1)
+            cursor.execute(*sql_table.update([sql_table.company], [value]))
 
     def get_rec_name(self, name):
         name = '[%s]' % self.code
@@ -63,6 +89,10 @@ class Asset(ModelSQL, ModelView):
     @staticmethod
     def default_type():
         return Transaction().context.get('type', '')
+
+    @staticmethod
+    def default_company():
+        return Transaction().context.get('company')
 
     def get_code_readonly(self, name):
         return True
