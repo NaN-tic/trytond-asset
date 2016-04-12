1d39704d195b49fda7e348f4be9854286351dce4
Guillem Barba <guillem@nan-tic.com>
2016-04-12 12:59:50 +0200

    PEP8 formatting

--- configuration.py
+++ configuration.py
@@ -1,5 +1,5 @@
-#This file is part of Tryton.  The COPYRIGHT file at the top level of
-#this repository contains the full copyright notices and license terms.
+# This file is part of Tryton.  The COPYRIGHT file at the top level of
+# this repository contains the full copyright notices and license terms.
 from trytond.model import ModelView, ModelSQL, ModelSingleton, fields
 
 __all__ = ['Configuration']
@@ -8,7 +8,6 @@ __all__ = ['Configuration']
 class Configuration(ModelSingleton, ModelSQL, ModelView):
     'Asset Configuration'
     __name__ = 'asset.configuration'
-
     asset_sequence = fields.Property(fields.Many2One('ir.sequence',
             'Asset Sequence', domain=[
                 ('code', '=', 'asset'),
