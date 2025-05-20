c98fc658e1ff0e11b552a61551afe8b5c11745f9
Raimon Esteve <raimonesteve@gmail.com>
2025-05-20 12:55:42 +0200

    FIX Removing deprecated args from setup

--- setup.py
+++ setup.py
@@ -46,15 +46,18 @@ for dep in info.get('depends', []):
         requires.append(get_require_version('%s_%s' % (prefix, dep)))
 requires.append(get_require_version('trytond'))
 
-tests_require = [get_require_version('proteus')]
-tests_require = [get_require_version('proteus')]
+tests_require = [
+    get_require_version('proteus'),
+]
+
 series = '%s.%s' % (major_version, minor_version)
 if minor_version % 2:
-    branch = 'master'
+    branch = 'default'
 else:
     branch = series
-    
+
 dependency_links = []
+
 if minor_version % 2:
     # Add development index for testing with proteus
     dependency_links.append('https://trydevpi.tryton.org/')
@@ -64,8 +67,9 @@ setup(name='%s_%s' % (PREFIX, MODULE),
     description='',
     long_description=read('README'),
     author='NaN·tic',
+    author_email='info@nan-tic.com',
     url='http://www.nan-tic.com/',
-    download_url="https://github.com/NaN-tic/trytond-%s" % MODULE,
+    download_url="https://bitbucket.org/nantic/trytond-%s" % MODULE,
     package_dir={'trytond.modules.%s' % MODULE: '.'},
     packages=[
         'trytond.modules.%s' % MODULE,
@@ -73,7 +77,7 @@ setup(name='%s_%s' % (PREFIX, MODULE),
         ],
     package_data={
         'trytond.modules.%s' % MODULE: (info.get('xml', [])
-            + ['tryton.cfg', 'view/*.xml', 'locale/*.po', 'tests/*.rst']),
+            + ['tryton.cfg', 'locale/*.po', 'tests/*.rst']),
         },
     classifiers=[
         'Development Status :: 5 - Production/Stable',
